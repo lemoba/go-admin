e1950f82b2c2b4d2aa040cbe06c8852b28d42250
ranen <189527031@qq.com>
2022-12-28 16:42:20 +0800

    feat: add template html

--- controller/test.go
+++ controller/test.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func registerTestRoutes() {
@@ -10,9 +11,25 @@ func registerTestRoutes() {
 }
 
 func handleTest(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("assets/week.html")
+	//t, _ := template.ParseFiles("assets/test.html")
+	//t.Execute(w, "hello")
 	//rand.Seed(time.Now().Unix())
 	//t.Execute(w, rand.Intn(10) > 5)
-	daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sta", "Sun"}
-	t.Execute(w, daysOfWeek)
+	//daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sta", "Sun"}
+	//t.Execute(w, daysOfWeek)
+
+	//t, _ := template.ParseFiles("assets/t1.html", "assets/t2.html")
+	//t.Execute(w, "hello world")
+
+	funcMap := template.FuncMap{"fdate": formatDate}
+	t := template.New("t3.html").Funcs(funcMap)
+
+	t.ParseFiles("assets/t3.html")
+	t.Execute(w, time.Now())
+
+}
+
+func formatDate(t time.Time) string {
+	layout := "2006-01-02 15:04:05"
+	return t.Format(layout)
 }
--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"html/template"
+	"github.com/lemoba/go-admin/controller"
 	"net/http"
 )
 
 func main() {
-	server := &http.Server{
-		Addr: "localhost:8080",
-	}
-	http.HandleFunc("/process", process)
-	server.ListenAndServe()
-}
-
-func process(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("assets/tmpl.html")
-	t.Execute(w, "Hello World!")
+	controller.RegisterRoutes()
+	http.ListenAndServe("localhost:8080", nil)
 }
